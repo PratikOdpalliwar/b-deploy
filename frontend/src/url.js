141f6c19fbd4b7bc064ed37febc5aa3da49b82a8
Pratik_Odpalliwar <pratikodpalliwar777@gmail.com>
2024-03-18 19:28:22 +0530

    test

---
+++ frontend/src/url.js
@@ -0,0 +1,11 @@
+// export const URL=import.meta.env.VITE_URL
+// export const IF=import.meta.env.VITE_IF
+
+
+// VITE_URL="http://localhost:5000"
+// VITE_IF="http://localhost:5000/images/"
+
+
+export const URL="http://localhost:5000"
+export const IF="http://localhost:5000/images/"
+
